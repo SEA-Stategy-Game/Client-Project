8d5275d6124b95887ad2ae4de972092d92185e60
JosiahDG94 <josiahdavidgroenhoej@live.dk>
2026-05-08 23:29:09 +0200

    changes to tree_placer.gd and main_menu.gd using the supplied package from Core-Systems

--- main_menu.gd
+++ main_menu.gd
@@ -3,18 +3,56 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var dict = {
+	  "current_tick": 16,
+	  "units": [
+		{
+		  "meta_values": {
+			"entity_id": -1,
+			"max_health": 100,
+			"player_id": 0,
+			"position": [-114.4198, -130.1099]
+		  },
+		  "path": [
+			[-129.8199, -39.1828],
+			[-114.8059, -127.8304],
+			[-113.368, -136.3203]
+		  ],
+		  "speed": 2000
+		}
+	  ],
+	  "modified_objects": [
+		{
+		  "meta_values": {
+			"entity_id": -1,
+			"max_health": 100,
+			"player_id": -1,
+			"position": [-128.0, -136.0]
+		  },
+		  "destroyed": true,
+		  "amount_left": 0
+		}
+	  ]
+	} 
+	dict = JSON.stringify(dict)
+	var dict_data = JSON.parse_string(dict)
+	_on_start_pressed(dict_data)
+	#pass # Replace with function body.
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/node_2d.tscn")
-	print("Start")
-	pass # Replace with function body.
+func _on_start_pressed(dict_data: Dictionary) -> void:
+	get_tree().change_scene_to_file("res://scenes/node_2d.tscn") # I don't know how to use this - Josiaht
+	var tick = dict_data["current_tick"]
+	var unit_info = dict_data["units"][0]["meta_values"]
+	var mod_objs = dict_data["modified_objects"][0]["meta_values"] 
+	var place_trees = TreePlacer.new() 
+	place_trees.fill_from_reference(mod_objs) 
+	#pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
--- scenes/tree_placer.gd
+++ scenes/tree_placer.gd
@@ -1,23 +1,37 @@
+class_name TreePlacer
 extends TileMapLayer
 
 @export var reference_layer: TileMapLayer
 @export var random_tiles = [1, 2, 3]
 
 func _ready() -> void:
-	randomize()
-	fill_from_reference()
+	#randomize()
+	#fill_from_reference()
+	pass
 
-func fill_from_reference() -> void:
+func fill_from_reference(tree_data: Dictionary) -> void:
 	if reference_layer == null:
 		print("Reference layer not set!")
 		return
 	
-	var rect = reference_layer.get_used_rect()
+	var pos = tree_data["position"]
+	var ref_tile = reference_layer.get_cell(Vector2i(int(pos[0]), int(pos[1])))
 	
-	for x in range(rect.position.x, rect.position.x + rect.size.x):
-		for y in range(rect.position.y, rect.position.y + rect.size.y):
-			var ref_tile = reference_layer.get_cell(Vector2i(x, y))
-			
-			if ref_tile != -1:
-				var tile_id = random_tiles[randi() % random_tiles.size()]
-				set_cell(Vector2i(x, y), tile_id)
+	if ref_tile != -1:
+		var tile_id = random_tiles[randi() % random_tiles.size()]
+		set_cell(Vector2i(int(pos[0]), int(pos[1])), tile_id)
+		
+#func fill_from_reference() -> void:
+	#if reference_layer == null:
+		#print("Reference layer not set!")
+		#return
+	#
+	#var rect = reference_layer.get_used_rect()
+#
+	#for x in range(rect.position.x, rect.position.x + rect.size.x):
+		#for y in range(rect.position.y, rect.position.y + rect.size.y):
+			#var ref_tile = reference_layer.get_cell(Vector2i(x, y))
+#
+			#if ref_tile != -1:
+				#var tile_id = random_tiles[randi() % random_tiles.size()]
+				#set_cell(Vector2i(x, y), tile_id)
