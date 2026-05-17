8aff1292f564df993c4426fea7c7101decebb750
Flavia Pezoti <flaviapezoti@gmail.com>
2026-05-17 10:57:58 +0200

    dinamically fetch the game room address using game room manager

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	LobbyClient.rooms_fetched.connect(_on_rooms_received)
+	LobbyClient.request_failed.connect(_on_rooms_failed)
 	pass # Replace with function body.
 	
 
@@ -12,7 +14,8 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/node_2d.tscn")
+	LobbyClient.list_game_rooms()
+	print("Fetching rooms...")
 
 	pass
 
@@ -23,3 +26,17 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 	print("Exit")
 	pass # Replace with function body.
+
+func _on_rooms_received(rooms: Array):
+	if rooms.size() > 0:
+		LobbyClient.selected_room = rooms[0]
+		print("Selected Room: ", LobbyClient.selected_room)
+	
+		get_tree().change_scene_to_file("res://scenes/node_2d.tscn")
+	else:
+		print("No rooms available.")
+
+
+
+func _on_rooms_failed(error_message: String):
+	print("Main Menu: Error fetching rooms -> ", error_message)
